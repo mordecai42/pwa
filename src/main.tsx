c3e2826d6f7642444fd0bd0c7cfd46540a1fb4a4
mordecai42 <jonathanivaan123@gmail.com>
2025-10-05 17:22:37 -0600

    pwa xd

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
